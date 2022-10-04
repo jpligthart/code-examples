300a9bdf634315bbe591d2226f8f991048bceab7
jpligthart <875812@fontys.nl>
2022-10-04 13:29:53 +0200

    Add to repo

---
+++ SingleFileExamples/Examples/ListReferenceExample.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SingleFileExamples.Examples
+{
+    public class ListReferenceIssueTester
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Hello World!");
+
+            var tester = new ExampleObject();
+
+            // Show list before manupilation
+            WriteListToConsole(tester.Values);
+
+            tester.Values.Add(4);
+
+            // Show list again and see extra value '4'
+            WriteListToConsole(tester.Values);
+        }
+
+        private void WriteListToConsole(IEnumerable<int> list)
+        {
+            Console.WriteLine("------");
+
+            foreach (int value in list)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine("------");
+        }
+
+    }
+
+    public class ExampleObject
+    {
+        private readonly List<int> _values = new();
+
+        public List<int> Values => _values;
+
+        public ExampleObject()
+        {
+            _values.Add(2);
+            _values.Add(3);
+        }
+
+    }
+}
---
+++ SingleFileExamples/Examples/LoopPerformanceMeasurement.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SingleFileExamples.Examples;
+
+public class LoopPerformanceMeasurement
+{
+    private const int MaxValue = 10_000_000;
+
+    private readonly List<int> _data = new();
+
+    public void Execute()
+    {
+        InitDataCollection();
+
+        int valueToSearch = MaxValue - 1;
+        string genericMessage = $"Find value '{valueToSearch}' using ";
+
+        TestMethodDefinition loopTester = new(_data, valueToSearch);
+
+        Console.WriteLine("Testing started: ");
+        ExecuteTestMethod($"{genericMessage} foreach", loopTester.TestFindWithForeach);
+        ExecuteTestMethod($"{genericMessage} for", loopTester.TestFindWithFor);
+        ExecuteTestMethod($"{genericMessage} while", loopTester.TestFindWithWhile);
+        ExecuteTestMethod($"{genericMessage} linq", loopTester.TestFindWithLinq);
+    }
+
+    private void ExecuteTestMethod(string message, Func<bool> testMethod)
+    {
+        Stopwatch stopwatch = new();
+
+        Console.WriteLine();
+        Console.WriteLine(message);
+
+        stopwatch.Start();
+        bool isFound = testMethod();
+        stopwatch.Stop();
+
+        PrintTimerResult(isFound, stopwatch);
+        stopwatch.Reset();
+    }
+
+    private void InitDataCollection()
+    {
+        for (int i = 0; i < MaxValue; i++)
+        {
+            _data.Add(i);
+        }
+    }
+
+    private void PrintTimerResult(bool isFound, Stopwatch stopwatch)
+    {
+        if (isFound)
+        {
+            Console.WriteLine($"Found value in {stopwatch.Elapsed} milliseconds");
+        }
+        else
+        {
+            Console.WriteLine($"Not Found! And search duration is {stopwatch.ElapsedMilliseconds} milliseconds");
+        }
+    }
+}
+
+public class TestMethodDefinition
+{
+    private readonly int _searchedValue;
+
+    private readonly List<int> _dataCollection;
+
+    public TestMethodDefinition(IEnumerable<int> dataCollection, int searchedValue)
+    {
+        _dataCollection = dataCollection.ToList();
+        _searchedValue = searchedValue;
+    }
+
+    private bool IsSearchedValue(int element)
+    {
+        return element == _searchedValue;
+    }
+
+    public bool TestFindWithFor()
+    {
+        for (int i = 0; i < _dataCollection.Count; i++)
+        {
+            if (IsSearchedValue(i)) return true;
+        }
+
+        return false;
+    }
+
+    public bool TestFindWithForeach()
+    {
+        foreach (int element in _dataCollection)
+        {
+            if (IsSearchedValue(element)) return true;
+        }
+
+        return false;
+    }
+
+    public bool TestFindWithLinq()
+    {
+        int? result = _dataCollection.FirstOrDefault(element => element == _searchedValue);
+        return result != null;
+    }
+
+    public bool TestFindWithWhile()
+    {
+        bool isFound = false;
+        int index = 0;
+        int element;
+
+        while (!isFound && index < _dataCollection.Count)
+        {
+            element = _dataCollection[index];
+            isFound = IsSearchedValue(element);
+
+            index++;
+        }
+
+        return isFound;
+    }
+
+
+}
